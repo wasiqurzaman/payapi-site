c5b8880f9fec1d637c6ae8c0ebc07f04fb36ea6b
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-08 21:40:04 +0530

    Enhance Header and Nav components with responsive design and improved layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export default function Header() {
   return (
-    <header className="flex items-center gap-[64px] max-w-[1110px] mx-auto">
+    <header className="flex items-center justify-between gap-[40px] md:gap-[64px] max-w-[1110px] mx-auto px-[24px] md:px-[40px] xl:px-0">
       <div className="flex items-center justify-center">
         <Image
           src="/assets/shared/desktop/logo.svg"
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const links = [
@@ -8,24 +9,36 @@ const links = [
 
 export default function Nav() {
   return (
-    <div className="flex items-center flex-1">
-      <ul className="flex items-center gap-[40px]">
-        {links.map((link, index) => (
-          <Link
-            key={index}
-            href={link.url}
-            className="text-secondary-sanJuanBlue font-bold capitalize"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
-      <Link
-        href="/schedule"
-        className="bg-primary text-linkWaterWhite font-secondary font-bold py-[14px] px-[26px] rounded-full ml-auto"
-      >
-        Schedule a Demo
-      </Link>
-    </div>
+    <>
+      <div className="hidden tab:flex items-center flex-1">
+        <ul className="flex items-center gap-[40px]">
+          {links.map((link, index) => (
+            <Link
+              key={index}
+              href={link.url}
+              className="text-sanJualBlueLight font-bold capitalize hover:text-sanJuanBlue transition-colors duration-300"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+        <Link
+          href="/schedule"
+          className="bg-primary text-linkWaterWhite font-secondary font-bold py-[14px] px-[20px] md:py-[14px] md:px-[26px] rounded-full ml-auto text-nowrap cursor-pointer hover:bg-charmPink transition-colors duration-300"
+        >
+          Schedule a Demo
+        </Link>
+      </div>
+      <div className="tab:hidden flex items-center">
+        <button>
+          <Image
+            src="/assets/shared/mobile/menu.svg"
+            alt="mobile nav icon"
+            width={30}
+            height={20}
+          />
+        </button>
+      </div>
+    </>
   );
 }
