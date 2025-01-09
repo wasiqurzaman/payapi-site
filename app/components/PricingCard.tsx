f392e6f1c644cfe39dc652b9f7905dda3bb04532
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-09 23:59:58 +0530

    Update PricingCard and Ready components for improved responsiveness and layout

--- app/components/PricingCard.tsx
+++ app/components/PricingCard.tsx
@@ -20,7 +20,7 @@ export default function PricingCard({ pricing }: Props) {
         <h2 className="h4 lg:h3-big text-primary lg:text-primary mb-[12px]  lg:mb-[16px]">
           {pricing.plan}
         </h2>
-        <p className="text-sanJuanBlueLight mb-2 max-w-[90%] tab:max-w-full">
+        <p className="text-sanJuanBlueLight mb-2 max-w-[90%] sm:max-w-[70%] tab:max-w-full">
           {pricing.desc}
         </p>
         <h3 className="h1-sm tab:h2 lg:h1-sm leading-[72px] mt-auto">
